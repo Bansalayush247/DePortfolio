411846c0b344035f090576969e3d7b03b41f40e6
Ayush Bansal <116891199+Bansalayush247@users.noreply.github.com>
2023-07-10 21:55:24 +0530

    github link update

--- src/components/handles/Handles.jsx
+++ src/components/handles/Handles.jsx
@@ -8,7 +8,7 @@ const Handles = () => {
     <section className='socials'>
       <a href="https://www.linkedin.com/in/bansalayush247/" target='_blank' rel="noopener noreferrer"><AiFillLinkedin className='icon' /></a>
       <a href="https://twitter.com/ayushbansal2407?lang=en" target='_blank' rel="noopener noreferrer"><AiFillTwitterSquare className='icon' /></a>
-      <a href="https://github.com/bansal247" target='_blank' rel="noopener noreferrer"><FaGithubSquare className='icon' />
+      <a href="https://github.com/bansalayush247" target='_blank' rel="noopener noreferrer"><FaGithubSquare className='icon' />
       </a>
 
 
